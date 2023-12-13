3e9e470b2f0c7b74f88216c0dced36b29b5188fd
Josiah Wiedenheft <jwiedenheft@gmail.com>
2023-12-13 17:47:43 -0500

    Add models for Tags

--- backend/finance_app/database/models.py
+++ backend/finance_app/database/models.py
@@ -1,7 +1,8 @@
 import enum
+from typing import List
 from . import db
-from sqlalchemy import DateTime, Enum, Integer, String
-from sqlalchemy.orm import Mapped, mapped_column
+from sqlalchemy import DateTime, Enum, ForeignKey, Integer, String
+from sqlalchemy.orm import Mapped, mapped_column, relationship
 from datetime import datetime
 
 
@@ -16,20 +17,33 @@ class Income(db.Model):
     id: Mapped[int] = mapped_column(Integer, primary_key=True)
     date: Mapped[datetime] = mapped_column(DateTime, nullable=False)
     create_date: Mapped[datetime] = mapped_column(
-        DateTime, nullable=False, default_factory=datetime.now
+        DateTime, nullable=False, default=datetime.now()
     )
     title: Mapped[str] = mapped_column(String(256), nullable=False, default="")
     total_amount: Mapped[int] = mapped_column(Integer, nullable=False, default=0)
     percent_spend: Mapped[int] = mapped_column(Integer, nullable=False, default=80)
 
 
-class Spend(db.Model):
+class Expense(db.Model):
     id: Mapped[int] = mapped_column(Integer, primary_key=True)
     date: Mapped[datetime] = mapped_column(DateTime, nullable=False)
     create_date: Mapped[datetime] = mapped_column(
-        DateTime, nullable=False, default_factory=datetime.now
+        DateTime, nullable=False, default=datetime.now()
     )
     title: Mapped[str] = mapped_column(String(256), nullable=False, default="")
     notes: Mapped[str] = mapped_column(String(4000), nullable=False, default="")
     amount: Mapped[int] = mapped_column(Integer, nullable=False, default=0)
     category: Mapped[Category] = mapped_column(Enum(Category))
+    tags: Mapped[List["ExpenseTag"]] = relationship()
+
+
+class Tag(db.Model):
+    id: Mapped[int] = mapped_column(Integer, primary_key=True)
+    name: Mapped[str] = mapped_column(String(20), nullable=False)
+
+
+class ExpenseTag(db.Model):
+    id: Mapped[int] = mapped_column(Integer, primary_key=True)
+    expense_id: Mapped[int] = mapped_column(ForeignKey(f"{Expense.__tablename__}.id"))
+    tag_id: Mapped[int] = mapped_column(ForeignKey(f"{Tag.__tablename__}.id"))
+    tag: Mapped["Tag"] = relationship()
--- backend/finance_app/main/routes.py
+++ backend/finance_app/main/routes.py
@@ -1,6 +1,15 @@
 from finance_app.main import main
+from finance_app.database.models import Expense
 
 
 @main.route("/")
 def index():
-    return "Hello world!"
+    spends = Expense.query.all()
+    out = ""
+    spend: Expense
+    for spend in spends:
+        out += f"{spend.title} | "
+        +f"Amount: {spend.amount} "
+        +f"Category: {spend.category} "
+        +f"Tags: {", ".join([t.tag.name for t in spend.tags])}<br>"
+    return out
