c3764f92ae39c7d5174844488387ff47819de049
Josiah Wiedenheft <jwiedenheft@gmail.com>
2024-05-08 18:21:08 -0400

    Rethink database structure

--- finance_app/models.py
+++ finance_app/models.py
@@ -1,6 +1,6 @@
 from typing import List
 from finance_app import db
-from sqlalchemy import DateTime, ForeignKey, Integer, String, text
+from sqlalchemy import DateTime, ForeignKey, Integer, String
 from sqlalchemy.orm import Mapped, mapped_column, relationship
 from datetime import datetime
 
@@ -11,15 +11,9 @@ class Category(db.Model):
     code: Mapped[str] = mapped_column(String(10), nullable=False, primary_key=True)
     title: Mapped[str] = mapped_column(String(60), nullable=False, default="")
     color: Mapped[str] = mapped_column(String(6), default="ffffff", nullable=False)
-    expenses: Mapped[List["Expense"]] = relationship()
 
-    default_income_percent: Mapped[int] = mapped_column(
-        Integer(), nullable=False, server_default=text("0")
-    )
-
-    category_incomes: Mapped[List["CategoryIncome"]] = relationship(
-        back_populates="category"
-    )
+    expenses: Mapped[List["Expense"]] = relationship(back_populates="category")
+    income: Mapped[List["Income"]] = relationship(back_populates="category")
 
     def balance(self):
         return self.income_total - self.expenses_total
@@ -30,7 +24,7 @@ class Category(db.Model):
 
     @property
     def income_total(self):
-        return sum([ci.amount for ci in self.category_incomes])
+        return sum([i.amount for i in self.income])
 
 
 class Income(db.Model):
@@ -44,44 +38,21 @@ class Income(db.Model):
     def date_string(self):
         return self.date.strftime("%m/%d/%y")
 
+    amount: Mapped[int] = mapped_column(Integer, nullable=False, default=0)
+
+    def formatted_amount(self) -> str:
+        return int_to_money(self.amount)
+
     notes: Mapped[str] = mapped_column(String(4000), nullable=False, default="")
 
     create_date: Mapped[datetime] = mapped_column(
         DateTime, nullable=False, default=datetime.now()
     )
 
-    @property
-    def total_amount(self):
-        return sum([c.amount for c in self.category_incomes])
-
-    def formatted_total_amount(self):
-        return int_to_money(self.total_amount)
-
-    category_incomes: Mapped[List["CategoryIncome"]] = relationship(
-        back_populates="income", cascade="all, delete-orphan"
-    )
-
-    def income_for_category(self, category: Category):
-        for ci in self.category_incomes:
-            if ci.category_code == category.code:
-                return ci
-        return None
-
-
-class CategoryIncome(db.Model):
-    id: Mapped[int] = mapped_column(Integer, primary_key=True)
-
-    income_id: Mapped[int] = mapped_column(ForeignKey(f"{Income.__tablename__}.id"))
-    income: Mapped["Income"] = relationship()
-
     category_code: Mapped[str] = mapped_column(
         ForeignKey(f"{Category.__tablename__}.code")
     )
-    category: Mapped["Category"] = relationship()
-    amount: Mapped[int] = mapped_column(Integer, nullable=False, default=0)
-
-    def formatted_amount(self):
-        return int_to_money(self.amount)
+    category: Mapped["Category"] = relationship(back_populates="income")
 
 
 class Expense(db.Model):
