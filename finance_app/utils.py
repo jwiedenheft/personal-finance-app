e51f3e47eb5e941940fdc34473d08124d4250f62
Josiah Wiedenheft <jwiedenheft@gmail.com>
2024-05-22 19:46:57 -0400

    Add make_csv_file util, which should have been in previous commit

--- finance_app/utils.py
+++ finance_app/utils.py
@@ -1,2 +1,21 @@
+import csv
+from io import BytesIO, StringIO
+
+
 def int_to_money(cents: int):
     return "$" + str(cents / 100)
+
+
+def make_csv_file(data: list[dict]) -> BytesIO:
+    if len(data) < 1:
+        return BytesIO()
+
+    # Write dictionary to string
+    with StringIO() as string_file:
+        csv_writer = csv.DictWriter(string_file, data[0].keys())
+        csv_writer.writerows(data)
+        string_file.seek(0)
+        csv_str = string_file.read()
+
+    # Create in-memory file using csv string
+    return BytesIO(csv_str.encode("utf-8"))
